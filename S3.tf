620220dd20728a81378ab530f5159f4db85522be
Akriti <akriti.gupta2009@gmail.com>
2022-07-15 20:33:24 +0200

    TF Demo

---
+++ S3.tf
@@ -0,0 +1,9 @@
+resource "aws_s3_bucket" "testbucket" {
+  bucket = "awseksbackup"
+
+  tags = {
+    "Name" : "My bucket"
+    "Env": "Dev",
+    "Video": "CloudQuickLabs"
+  }
+}
--- main.tf
+++ main.tf
@@ -1,28 +1,20 @@
+## backend data for terraform
 terraform {
   required_providers {
     aws = {
-      source  = "hashicorp/aws"
-      version = "~> 3.0"
+      source = "hashicorp/aws"
+    }
+  }
+
+  backend "remote" {
+  organization = "CloudQuickLabs"
+
+    workspaces {
+      name = "AWSBackup"
     }
   }
 }
-variable "iam_user_name_prefix" {
-  type    = string
-  default = "my_iam_user"
-}
-# configure the AWS Provider
+
 provider "aws" {
-  profile    = "default"
-  region     = "us-east-1"
-  access_key = "AKIAXDBJK4VEGYZ7YNNB"
-  secret_key = "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"
-
-}
-#IAM User
-
-resource "aws_iam_user" "my_iam_users" {
-  count = 1
-  name  = "${var.iam_user_name_prefix}_${count.index}"
-
-
+  region = "us-east-1"
 }
